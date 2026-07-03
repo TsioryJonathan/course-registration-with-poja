55bd4fb94f42e74e91ce0332929aebb4b5052380
NickyHariniaina <ferrissushi@gmail.com>
2026-07-03 08:47:48 +0300

    feat: implement SendEmailRequestedService event handler

---
+++ src/main/java/org/onlydevs/registration/service/event/SendEmailRequestedService.java
@@ -0,0 +1,26 @@
+package org.onlydevs.registration.service.event;
+
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.onlydevs.registration.PojaGenerated;
+import org.onlydevs.registration.endpoint.event.model.SendEmailRequested;
+import org.onlydevs.registration.mail.Email;
+import org.onlydevs.registration.mail.Mailer;
+import org.springframework.stereotype.Service;
+
+@PojaGenerated
+@Service
+@AllArgsConstructor
+public class SendEmailRequestedService implements Consumer<SendEmailRequested> {
+  private final Mailer mailer;
+
+  @SneakyThrows
+  @Override
+  public void accept(SendEmailRequested sendEmailRequested) {
+    InternetAddress recipientAddress = new InternetAddress(sendEmailRequested.getTo());
+    mailer.accept(new Email(recipientAddress, List.of(), List.of(), "", "... world!", List.of()));
+  }
+}
