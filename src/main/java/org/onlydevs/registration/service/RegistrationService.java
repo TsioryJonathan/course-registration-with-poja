0238ca3e3b89a07fd25f2e4b2aff071043a57e14
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-07-03 09:28:01 +0300

    fix: commit concerned changes

--- src/main/java/org/onlydevs/registration/service/RegistrationService.java
+++ src/main/java/org/onlydevs/registration/service/RegistrationService.java
@@ -1,18 +1,21 @@
 package org.onlydevs.registration.service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
-
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.onlydevs.registration.endpoint.event.EventProducer;
+import org.onlydevs.registration.endpoint.event.model.SendEmailRequested;
+import org.onlydevs.registration.model.Course;
 import org.onlydevs.registration.model.Registration;
+import org.onlydevs.registration.model.User;
 import org.onlydevs.registration.repository.CourseRepository;
 import org.onlydevs.registration.repository.EmailRepository;
 import org.onlydevs.registration.repository.RegistrationRepository;
 import org.onlydevs.registration.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 @Service
 @AllArgsConstructor
@@ -22,12 +25,21 @@ public class RegistrationService {
   private final EmailRepository emailRepository;
   private final UserRepository userRepository;
   private final CourseRepository courseRepository;
+  private final EventProducer<SendEmailRequested> eventProducer;
 
   @Transactional
-  public void register(UUID userId, UUID courseId){
-    var user = userRepository.findById(userId).orElseThrow(() -> NoSuchElementException("User with id:" + id +" does not exist."));
-    var course = courseRepository.findById(courseId).orElseThrow(() -> NoSuchElementException("Course with id: " + id + "does not exist"));
-    var registration = Registration.builder().user(user).course(course).build();
-    registrationRepository.save(i)
+  public void register(UUID userId, UUID courseId) {
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> NoSuchElementException("User with id:" + id + " does not exist."));
+    Course course =
+        courseRepository
+            .findById(courseId)
+            .orElseThrow(() -> NoSuchElementException("Course with id: " + id + "does not exist"));
+    Registration registration = Registration.builder().user(user).course(course).build();
+    registrationRepository.save(registration);
+    var event = SendEmailRequested.builder().to(user.getEmail()).build();
+    eventProducer.accept(List.of(event));
   }
 }
