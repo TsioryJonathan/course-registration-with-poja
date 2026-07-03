82e5d0828e950a89a6e52490ebd795ebacae41c2
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-07-03 09:50:55 +0300

    feat: add missing controller

--- src/main/java/org/onlydevs/registration/endpoint/rest/controller/RegistrationController.java
+++ src/main/java/org/onlydevs/registration/endpoint/rest/controller/RegistrationController.java
@@ -1,8 +1,24 @@
 package org.onlydevs.registration.endpoint.rest.controller;
 
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.onlydevs.registration.service.RegistrationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/register")
-public class RegistrationController {}
+@RequiredArgsConstructor
+public class RegistrationController {
+  private final RegistrationService registrationService;
+
+  @PostMapping
+  public ResponseEntity<String> registrer(@RequestParam UUID userId, @RequestParam UUID courseId) {
+    registrationService.register(userId, courseId);
+    return ResponseEntity.status(HttpStatus.CREATED).body("Successfull registration");
+  }
+}
