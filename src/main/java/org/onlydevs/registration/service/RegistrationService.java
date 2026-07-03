dfc8bf05d3c5ce477d44948b43ec4bc7a2fabd12
Tsiory Jonathan <167012196+TsioryJonathan@users.noreply.github.com>
2026-07-03 09:40:59 +0300

    Feat/async email sending (#3)
    * feat: implement SendEmailRequested event model
    
    * feat: implement SendEmailRequestedService event handler
    
    * chore: customize the email
    
    * feat: event service flow
    
    * fix: commit concerned changes
    
    * chore: add dynamic body to email event
    
    * fix: correct Optional error
    
    * chore: format
    
    * feat: clearer email body
    
    * feat: save email in databases
    
    ---------
    
    Co-authored-by: NickyHariniaina <ferrissushi@gmail.com>

---
+++ src/main/java/org/onlydevs/registration/service/RegistrationService.java
@@ -0,0 +1,49 @@
+package org.onlydevs.registration.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.onlydevs.registration.endpoint.event.EventProducer;
+import org.onlydevs.registration.endpoint.event.model.SendEmailRequested;
+import org.onlydevs.registration.model.EmailUser;
+import org.onlydevs.registration.model.Registration;
+import org.onlydevs.registration.repository.CourseRepository;
+import org.onlydevs.registration.repository.EmailRepository;
+import org.onlydevs.registration.repository.RegistrationRepository;
+import org.onlydevs.registration.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RegistrationService {
+  private final RegistrationRepository registrationRepository;
+  private final EmailRepository emailRepository;
+  private final UserRepository userRepository;
+  private final CourseRepository courseRepository;
+  private final EventProducer<SendEmailRequested> eventProducer;
+
+  @Transactional
+  public void register(UUID userId, UUID courseId) {
+    var user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(
+                () -> new NoSuchElementException("User with id:" + userId + " does not exist."));
+    var course =
+        courseRepository
+            .findById(courseId)
+            .orElseThrow(
+                () -> new NoSuchElementException("Course with id: " + courseId + "does not exist"));
+    var registration = Registration.builder().user(user).course(course).build();
+    var savedRegistration = registrationRepository.save(registration);
+    emailRepository.save(EmailUser.builder().registration(savedRegistration).build());
+    var event =
+        SendEmailRequested.builder()
+            .to(user.getEmail())
+            .body("Successful registration for course + " + course.getTitle())
+            .build();
+    eventProducer.accept(List.of(event));
+  }
+}
