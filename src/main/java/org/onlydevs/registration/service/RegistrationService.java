100e92f5810f9e621d25bb0380ce4249e3a6fe42
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-07-06 10:36:16 +0300

    fix: create a temp file before uploading to S3

--- src/main/java/org/onlydevs/registration/service/RegistrationService.java
+++ src/main/java/org/onlydevs/registration/service/RegistrationService.java
@@ -1,13 +1,12 @@
 package org.onlydevs.registration.service;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.onlydevs.registration.endpoint.event.EventProducer;
 import org.onlydevs.registration.endpoint.event.model.SendEmailRequested;
 import org.onlydevs.registration.file.bucket.BucketComponent;
@@ -32,6 +31,7 @@ public class RegistrationService {
   private final BucketComponent bucketComponent;
 
   @Transactional
+  @SneakyThrows
   public void register(UUID userId, UUID courseId) {
     var user =
         userRepository
@@ -42,41 +42,39 @@ public class RegistrationService {
         courseRepository
             .findById(courseId)
             .orElseThrow(
-                () -> new NoSuchElementException("Course with id: " + courseId + "does not exist"));
+                () ->
+                    new NoSuchElementException("Course with id: " + courseId + " does not exist"));
+
     var registration = Registration.builder().user(user).course(course).build();
     var savedRegistration = registrationRepository.save(registration);
     emailRepository.save(EmailUser.builder().registration(savedRegistration).build());
+
     var pdf = pdfGenerationService.generateInvoice(registration);
-    var key = "invoice" + userId + "-" + courseId + ".pdf";
-    File file;
-    try {
-      file = byteArrayToFile(pdf, key);
-    } catch (IOException exception) {
-      throw new RuntimeException("Failed to create file " + exception.getMessage());
+    var bucketKey = "invoice-" + userId + "-" + courseId + ".pdf";
+
+    var tempFile = File.createTempFile("invoice-", ".pdf");
+
+    try (var fos = new java.io.FileOutputStream(tempFile)) {
+      fos.write(pdf);
     }
-    String url = uploadToS3(file, key);
+
+    bucketComponent.upload(tempFile, bucketKey);
+
+    if (tempFile.exists()) {
+      tempFile.delete();
+    }
+
+    String url = bucketComponent.presign(bucketKey, Duration.ofHours(1)).toString();
+
     var event =
         SendEmailRequested.builder()
             .to(user.getEmail())
             .body(
-                "Successful registration for course + "
+                "Successful registration for course "
                     + course.getTitle()
-                    + "you can download your invoice here "
+                    + " you can download your invoice here "
                     + url)
             .build();
     eventProducer.accept(List.of(event));
-  }
-
-  public String uploadToS3(File file, String fileName) {
-    bucketComponent.upload(file, fileName);
-    return bucketComponent.presign(fileName, Duration.ofHours(1)).toString();
-  }
-
-  public File byteArrayToFile(byte[] bytes, String filePath) throws IOException {
-    File file = new File(filePath);
-    try (FileOutputStream fos = new FileOutputStream(file)) {
-      fos.write(bytes);
-    }
-    return file;
   }
 }
