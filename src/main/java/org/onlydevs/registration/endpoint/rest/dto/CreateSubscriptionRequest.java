b70b796b0095f220fda245285c0163b1f090f40f
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-07-06 09:16:14 +0300

    refactor: use request body instead of request param

---
+++ src/main/java/org/onlydevs/registration/endpoint/rest/dto/CreateSubscriptionRequest.java
@@ -0,0 +1,17 @@
+package org.onlydevs.registration.endpoint.rest.dto;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CreateSubscriptionRequest {
+  private UUID userId;
+}
